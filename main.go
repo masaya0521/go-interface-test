ea1d64d7321e20eec4a9203b0061ff134dbb2843
masaya0521 <kaji0521km@gmail.com>
2022-07-01 00:07:49 +0900

    interface

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+type User interface{
+	toStoring(s string)
+}
+
+type Hoge struct{
+	user string
+}
+
+func (h Hoge)toStoring(s string){
+	fmt.Println(s)
+}
+
 func main() {
-	fmt.Println("test")
+	var hoge Hoge = Hoge{user: "man"}
+	hoge.toStoring("test")
+
+	var user User 
+	user = hoge
+	user.toStoring("test2")
 }
